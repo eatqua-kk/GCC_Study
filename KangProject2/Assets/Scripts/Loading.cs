a1ed44c4c24f4851cef02bef5ad4e7f1a25e608d
DESKTOP-MLEQBNE\kang <kangtoe99@gmail.com>
2018-05-24 22:19:01 +0900

    no message

---
+++ KangProject2/Assets/Scripts/Loading.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+
+	public Image fill;
+	public GameObject setting;
+	public GameObject disable;
+
+	private float fillFloat = 0.01f;
+	private bool isFull = false;
+	private bool isActive = false;
+
+	public void ButtonOnClickSetting()
+	{
+		setting.SetActive (true);
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isFull == true)
+        {
+			return; // 종료
+		}
+
+		if (fill.fillAmount == 1)
+        {
+			isFull = true;
+			Debug.Log (isFull);
+            setting.SetActive(true);
+			disable.SetActive (false);
+        }
+
+		Debug.Log (fill.fillAmount);
+		fill.fillAmount += fillFloat;
+	}
+}
