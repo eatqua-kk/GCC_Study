baaa0acddc9307b94b94c740f53bb62c79565c58
이현민 <kun3650@gmail.com>
2018-06-06 11:12:37 +0900

    유티니 과제

---
+++ 2018_ProjectFile/sein1423/유니티/Assets/script/asdf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class asdf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click_S()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("asdf");
+    }
+}
