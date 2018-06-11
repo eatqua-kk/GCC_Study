14ef2bcb0774ca3956f203ef0df3de874d6500ac
DESKTOP-MLEQBNE\kang <kangtoe99@gmail.com>
2018-06-11 20:36:46 +0900

    no message

---
+++ 2018_ProjectFile/kangtoe/SS_tutorials/Assets/_Complete-Game/Scripts/PlayerContorller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorller : MonoBehaviour
+{
+
+    public int Speed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+
+        transform.position = new Vector3(
+        Mathf.Clamp(transform.position.x, -5.0f, 5.0f),
+        Mathf.Clamp(transform.position.y, -3.0f, 5.0f), 0);
+    }
+}
